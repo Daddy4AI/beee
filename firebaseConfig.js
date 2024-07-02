047113c633ae36f7a17a342bc9c1503b0770dbcb
Daddy4AI <alanqwyuan@gmail.com>
2024-07-02 21:49:37 +0800

    init

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+// firebaseConfig.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBlDcn2xJTWTVftGEOptBOWLFlY4LaS-YQ",
+  authDomain: "spellingbee-935f5.firebaseapp.com",
+  databaseURL: "https://spellingbee-935f5-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "spellingbee-935f5",
+  storageBucket: "spellingbee-935f5.appspot.com",
+  messagingSenderId: "1037070024462",
+  appId: "1:1037070024462:web:59fad718924cc57371a15f",
+  measurementId: "G-4672SVYH7K"
+};
+
+export default firebaseConfig;
